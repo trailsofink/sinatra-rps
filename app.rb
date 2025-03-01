549e1c560aeb160e60581d55751ed4cc095ede64
Joel Hunt <92796238+trailsofink@users.noreply.github.com>
2025-03-01 19:34:11 +0000

    rock,paper,scissors

--- app.rb
+++ app.rb
@@ -1,9 +1,50 @@
 require "sinatra"
 require "sinatra/reloader"
 
+npc = ["rock", "paper", "scissors"]
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:index)
 end
+
+get("/rock") do
+  @pc = "rock"
+  @npc_plays = npc.sample
+  case @npc_plays
+  when "rock"
+    @result = "We tied!"
+  when "paper"
+    @result = "We lost!"
+  when "scissors"
+    @result = "We won!"
+  end
+  erb(:game)
+end
+
+get("/paper") do
+  @pc = "paper"
+  @npc_plays = npc.sample
+  case @npc_plays
+  when "rock"
+    @result = "We lost!"
+  when "paper"
+    @result = "We tied!"
+  when "scissors"
+    @result = "We won!"
+  end
+  erb(:game)
+end
+
+get("/scissors") do
+  @pc = "scissors"
+  @npc_plays = npc.sample
+  case @npc_plays
+  when "rock"
+    @result = "We lost!"
+  when "paper"
+    @result = "We won!"
+  when "scissors"
+    @result = "We tied!"
+  end
+  erb(:game)
+end 
