ed36c0112eadf6593e5f52ac2ac09e835fe62b7a
Joel Hunt <92796238+trailsofink@users.noreply.github.com>
2025-03-02 19:07:09 +0000

    refactored dynamic params

--- app.rb
+++ app.rb
@@ -7,44 +7,63 @@ get("/") do
   erb(:index)
 end
 
-get("/rock") do
-  @pc = "rock"
-  @npc_plays = npc.sample
-  case @npc_plays
+get("/:player_choice") do
+  @pc = params.fetch("player_choice").downcase
+  @npc = npc.sample
+
+  case @pc
   when "rock"
-    @result = "We tied!"
+    if @npc == "rock"
+      @result = "We tied!"
+    elsif @npc == "paper"
+      @result = "We lost!"
+    elsif @npc == "scissors"
+      @result = "We won!"
+    end
   when "paper"
-    @result = "We lost!"
+    if @npc == "paper"
+      @result = "We tied!"
+    elsif @npc == "scissors"
+      @result = "We lost!"
+    elsif @npc == "rock"
+      @result = "We won!"
+    end
   when "scissors"
-    @result = "We won!"
+    if @npc == "scissors"
+      @result = "We tied!"
+    elsif @npc == "rock"
+      @result = "We lost!"
+    elsif @npc == "paper"
+      @result = "We won!"
+    end
   end
   erb(:game)
 end
 
-get("/paper") do
-  @pc = "paper"
-  @npc_plays = npc.sample
-  case @npc_plays
-  when "rock"
-    @result = "We lost!"
-  when "paper"
-    @result = "We tied!"
-  when "scissors"
-    @result = "We won!"
-  end
-  erb(:game)
-end
+# get("/paper") do
+#   @pc = "paper"
+#   @npc_plays = npc.sample
+#   case @npc_plays
+#   when "rock"
+#     @result = "We lost!"
+#   when "paper"
+#     @result = "We tied!"
+#   when "scissors"
+#     @result = "We won!"
+#   end
+#   erb(:game)
+# end
 
-get("/scissors") do
-  @pc = "scissors"
-  @npc_plays = npc.sample
-  case @npc_plays
-  when "rock"
-    @result = "We lost!"
-  when "paper"
-    @result = "We won!"
-  when "scissors"
-    @result = "We tied!"
-  end
-  erb(:game)
-end 
+# get("/scissors") do
+#   @pc = "scissors"
+#   @npc_plays = npc.sample
+#   case @npc_plays
+#   when "rock"
+#     @result = "We lost!"
+#   when "paper"
+#     @result = "We won!"
+#   when "scissors"
+#     @result = "We tied!"
+#   end
+#   erb(:game)
+# end
